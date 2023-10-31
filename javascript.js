99a32a4724dca4c754b0948832ed2949c9f3f9fb
dubbed14 <117707020+dubbed14@users.noreply.github.com>
2023-10-31 12:33:24 +0200

    add computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log("Hello!");
+function getComputerChoice() {
+    let compChoice = Math.random() * 10;
+    console.log(compChoice);
+    if (compChoice <= 3 ) {
+        console.log("rock!")
+    } else if (compChoice <= 7) {
+        console.log("paper!")
+    } else {
+        console.log("scissors!")
+    }
+}
+
+getComputerChoice();
