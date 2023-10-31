63185e5fcdeaeb30f31f3f8e457f39a9003d19a2
dubbed14 <117707020+dubbed14@users.noreply.github.com>
2023-10-31 14:11:29 +0200

    add player input and function for calculation

--- javascript.js
+++ javascript.js
@@ -3,11 +3,49 @@ function getComputerChoice() {
     console.log(compChoice);
     if (compChoice <= 3 ) {
         console.log("rock!")
+        return "rock";
     } else if (compChoice <= 7) {
         console.log("paper!")
+        return "paper";
     } else {
         console.log("scissors!")
+        return "scissors";
     }
 }
 
-getComputerChoice();
+function oneRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "rock") {
+        console.log("tie!");
+        return "tie!";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log("computer wins!");
+        return "computer wins!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log("player wins!");
+        return "player wins!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("player wins!");
+        return "player wins!";
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+        console.log("tie!");
+        return "tie!";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        console.log("computer wins!");
+        return "computer wins!";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        console.log("computer wins!");
+        return "computer wins!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("player wins!");
+        return "player wins!";
+    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        console.log("tie!");
+        return "tie!";
+    }
+}
+
+let playerSelection = prompt("rock, paper, scissors?");
+
+let gameResult = oneRound(playerSelection, getComputerChoice());
+
+//getComputerChoice();
