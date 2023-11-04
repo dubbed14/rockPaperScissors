dabf9879183dd654b19f4acfc1374658f9bec644
dubbed14 <117707020+dubbed14@users.noreply.github.com>
2023-11-04 12:46:18 +0200

    remove commented out code

--- javascript.js
+++ javascript.js
@@ -69,6 +69,4 @@ function game() {
 
 game();
 
-//let playerSelection = prompt("rock, paper, scissors?");
-//let gameResult = oneRound(playerSelection, getComputerChoice());
-//getComputerChoice();
+
